b80c3ac20cac7f2ad84066a6d02f30430cac500d
ccohane <ccohane@gmail.com>
2019-01-18 14:49:28 -0500

    Adding an app.py to try out interactivity with dash and plotly

--- run/src/routes.py
+++ run/src/routes.py
@@ -4,6 +4,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
+from dash.dependencies import Input, Output
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
