6aa56561b85c5691036157f8eb1a5a851e97018d
ccohane <ccohane@gmail.com>
2019-01-18 13:32:04 -0500

    Messing around with plotly

--- run/src/routes.py
+++ run/src/routes.py
@@ -7,21 +7,44 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello My Friend'),
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
 
-    html.Div(children='''
+app.layout = html.Div(style={'backgroundColor': colors['background']},children=[
+    html.H1(
+        children='Hello My Friend',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+
+    html.Div(
+        children='''
         I am creating a application with dash to visualize Overwatch Statistcs
-    '''),
+        ''',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
 
     dcc.Graph(
-        id='example-graph',
+        id='example-graph-2',
         figure={
             'data': [
                 {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
             ],
             'layout': {
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                },
                 'title': 'Dash Data Visualization'
             }
         }
