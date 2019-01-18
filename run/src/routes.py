891a23f259e72e87cc2823ca8a5f9efb733f81b5
ccohane <ccohane@gmail.com>
2019-01-18 13:46:11 -0500

    Adding a table to the page

--- run/src/routes.py
+++ run/src/routes.py
@@ -2,6 +2,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -11,6 +12,25 @@ colors = {
     'background': '#111111',
     'text': '#7FDBFF'
 }
+
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/'
+    'c78bf172206ce24f77d6363a2d754b59/raw/'
+    'c353e8ef842413cae56ae3920b8fd78468aa4cb2/'
+    'usa-agricultural-exports-2011.csv')
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
+
 
 app.layout = html.Div(style={'backgroundColor': colors['background']},children=[
     html.H1(
@@ -48,7 +68,13 @@ app.layout = html.Div(style={'backgroundColor': colors['background']},children=[
                 'title': 'Dash Data Visualization'
             }
         }
-    )
+    ),
+
+    html.Div(style={'background': 'white'} ,children=[
+        html.H4(children='US Agriculture Exports (2011)'),
+        generate_table(df)
+    ])
+
 ])
 
 if __name__ == '__main__':
