27e599ffba9fd111b1192378b79ebfed45cb91c0
ccohane <ccohane@gmail.com>
2019-01-24 19:57:56 -0500

    Computing aggregations upfront

--- run/src/storing_data_in_hidden_Browser.py
+++ run/src/storing_data_in_hidden_Browser.py
@@ -5,37 +5,51 @@ import numpy as np
 import pandas as pd
 from dash.dependencies import Input, Output
 
-global_df = pd.read_csv('...')
-app.layout = html.Div([
-    dcc.Graph(id='graph'),
-    html.Table(id='table'),
-    dcc.Dropdown(id='dropdown'),
 
-    # Hidden div inside the app that stores the intermediate value
-    html.Div(id='intermediate-value', style={'display': 'none'})
-])
-
-@app.callback(Output('intermediate-value', 'children'), [Input('dropdown', 'value')])
+@app.callback(
+    Output('intermediate-value', 'children'),
+    [Input('dropdown', 'value')])
 def clean_data(value):
-     # some expensive clean data step
+     # an expensive query step
      cleaned_df = your_expensive_clean_or_compute_step(value)
 
-     # more generally, this line would be
-     # json.dumps(cleaned_df)
-     return cleaned_df.to_json(date_format='iso', orient='split')
+     # a few filter steps that compute the data
+     # as it's needed in the future callbacks
+     df_1 = cleaned_df[cleaned_df['fruit'] == 'apples']
+     df_2 = cleaned_df[cleaned_df['fruit'] == 'oranges']
+     df_3 = cleaned_df[cleaned_df['fruit'] == 'figs']
 
-@app.callback(Output('graph', 'figure'), [Input('intermediate-value', 'children')])
-def update_graph(jsonified_cleaned_data):
+     datasets = {
+         'df_1': df_1.to_json(orient='split', date_format='iso'),
+         'df_2': df_2.to_json(orient='split', date_format='iso'),
+         'df_3': df_3.to_json(orient='split', date_format='iso'),
+     }
 
-    # more generally, this line would be
-    # json.loads(jsonified_cleaned_data)
-    dff = pd.read_json(jsonified_cleaned_data, orient='split')
+     return json.dumps(datasets)
 
-    figure = create_figure(dff)
+@app.callback(
+    Output('graph', 'figure'),
+    [Input('intermediate-value', 'children')])
+def update_graph_1(jsonified_cleaned_data):
+    datasets = json.loads(jsonified_cleaned_data)
+    dff = pd.read_json(datasets['df_1'], orient='split')
+    figure = create_figure_1(dff)
     return figure
 
-@app.callback(Output('table', 'children'), [Input('intermediate-value', 'children')])
-def update_table(jsonified_cleaned_data):
-    dff = pd.read_json(jsonified_cleaned_data, orient='split')
-    table = create_table(dff)
-    return table
+@app.callback(
+    Output('graph', 'figure'),
+    [Input('intermediate-value', 'children')])
+def update_graph_2(jsonified_cleaned_data):
+    datasets = json.loads(jsonified_cleaned_data)
+    dff = pd.read_json(datasets['df_2'], orient='split')
+    figure = create_figure_2(dff)
+    return figure
+
+@app.callback(
+    Output('graph', 'figure'),
+    [Input('intermediate-value', 'children')])
+def update_graph_3(jsonified_cleaned_data):
+    datasets = json.loads(jsonified_cleaned_data)
+    dff = pd.read_json(datasets['df_3'], orient='split')
+    figure = create_figure_3(dff)
+    return figure
