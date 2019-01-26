c10cc571cc7dcc3288884e45cd05f7d23c2946eb
ccohane <ccohane@gmail.com>
2019-01-25 21:08:01 -0500

    Working on making sliders and text areas

---
+++ run/src/Dash_core_components.py
@@ -0,0 +1,37 @@
+import dash_core_components as dcc
+
+dcc.Dropdown(
+    options=[
+        {'label': 'New York City', 'value': 'NYC'},
+        {'label': 'Montréal', 'value': 'MTL'},
+        {'label': 'San Francisco', 'value': 'SF'}
+    ],
+    multi=True,
+    value="MTL"
+)
+
+dcc.Slider(
+    min=0,
+    max=9,
+    marks={i: 'Label {}'.format(i) for i in range(10)},
+    value=5,
+)
+
+dcc.RangeSlider(
+    marks={i: 'Label {}'.format(i) for i in range(-5, 7)},
+    min=-5,
+    max=6,
+    value=[-3, 4]
+)
+
+dcc.Input(
+    placeholder='Enter a value...',
+    type='text',
+    value=''
+)
+
+dcc.Textarea(
+    placeholder='Enter a value...',
+    value='This is a TextArea component',
+    style={'width': '100%'}
+)
