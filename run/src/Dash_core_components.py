917c7dc92d0f6a9bcd5b63e348bb901cbed0510d
ccohane <ccohane@gmail.com>
2019-01-25 21:21:10 -0500

    Working with interactive dash tables

--- run/src/Dash_core_components.py
+++ run/src/Dash_core_components.py
@@ -2,18 +2,27 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-
+from datetime import datetime as dt
+import dash_table
+import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
     html.Div(dcc.Input(id='input-box', type='text')),
     html.Button('Submit', id='button'),
     html.Div(id='output-container-button',
-             children='Enter a value and press submit')
+             children='Enter a value and press submit'),
 
     html.Div(
+        dash_table.DataTable(
+        id='table',
+        columns=[{"name": i, "id": i} for i in df.columns],
+        data=df.to_dict("rows"),
+)
         dcc.Dropdown(
             options=[
                 {'label': 'New York City', 'value': 'NYC'},
@@ -69,6 +78,12 @@ app.layout = html.Div([
             value='MTL',
             labelStyle={'display': 'inline-block'}
         ),
+        dcc.DatePickerRange(
+            id='date-picker-range',
+            start_date=dt(1997, 5, 3),
+            end_date_placeholder_text='Select a date!'
+        ),
+
     )
 ])
 
