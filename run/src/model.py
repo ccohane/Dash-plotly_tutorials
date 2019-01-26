55708bd414fb3bb8310c57070eb061c5e55e9213
ccohane <ccohane@gmail.com>
2019-01-25 21:55:40 -0500

    Working on API calls for overwath

--- run/src/model.py
+++ run/src/model.py
@@ -1,3 +1,13 @@
 #!usr/lib/bin/activate
 
 #Model to handle backend logic
+
+import pandas as pd
+import json, jsonify, requests, pprint
+
+url = "https://api.overwatchleague.com/stats/matches/10528/maps/1"
+
+response=json.loads(requests.get(url).text)
+players = response['teams'][0]['players']
+for x in players:
+    pprint.pprint(x)
